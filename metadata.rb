ef3ea4fd518d8bcc9781538647b8f158d6c1a1ec
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2014-09-30 15:24:52 -1000

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'oracle'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures oracle'
+long_description 'Installs/Configures oracle'
+version          '0.1.0'
+
